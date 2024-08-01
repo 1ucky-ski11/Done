e551fe2b6a4b03a74bddfc28197fd2b27c442b65
1ucky-ski11 <umarbek07210@gmail.com>
2024-08-01 12:32:04 +0500

    Object

---
+++ JavaScript.js
@@ -0,0 +1,145 @@
+'use strict'
+// const car ={
+//     name:"BMW",
+//     year:2016
+// }
+// console.log(car)
+//
+// const phone ={
+//     name:"android",
+//     year:2024
+// }
+// console.log(phone)
+//
+// const country ={
+//     name:USA
+// }
+// console.log(country)
+//
+// const product ={
+//     name:"gun",
+//     year:2000
+// }
+// console.log(product)
+//
+// const computer ={
+//     name:"ASUS",
+//     year:2020
+// }
+// console.log(computer)
+//
+// const boy ={
+//     name:"Jhon",
+//     born:2005
+// }
+// console.log(boy)
+
+// const school ={
+//     name:"first School",
+//     year:1999
+// }
+// console.log(school)
+//
+// const girl ={
+//     name:"Ssrah",
+//     born:2006
+// }
+// console.log(girl)
+//
+// const country ={
+//     name:"Uzbekistan"
+// }
+// console.log(country)
+//
+// const  game ={
+//     name:"CS-1.6",
+//     year:2004
+// }
+// console.log(game)
+//
+// const computer ={
+//     name:"Macbook",
+//     year:2022
+// }
+// console.log(computer)
+//
+// const plant ={
+//     name:"flower",
+//     type:rare
+// }
+// console.log(plant)
+
+
+
+// 12 ta
+
+
+// const house ={
+//     name:"modern",
+//     year:2016
+// }
+// console.log(house)
+//
+// const room ={
+//     name:"it ",
+//     year:2030
+// }
+// console.log(room)
+//
+// const country ={
+//     name:French
+// }
+// console.log(country)
+//
+// const tv ={
+//     name:"artel",
+//     year:2020
+// }
+// console.log(tv)
+//
+// const computer ={
+//     name:"hp",
+//     year:2024
+// }
+// console.log(computer)
+//
+// const friend={
+//     name:"James",
+//     born:2000
+// }
+// console.log(friend)
+
+// const fruit ={
+//     name:"Apple",
+//     taste:delicious
+// }
+// console.log(fruit)
+//
+// const girl ={
+//     name:"Ammy",
+//     born:2001
+// }
+// console.log(girl)
+//
+// const country ={
+//     name:"Japan"
+// }
+// console.log(country)
+//
+// const  game ={
+//     name:"PES 2017",
+//     year:2016
+// }
+// console.log(game)
+//
+// const computer ={
+//     name:"AOS",
+//     year:2019
+// }
+// console.log(computer)
+//
+// const plant ={
+//     name:"flower",
+//     type:rare
+// }
+// console.log(plant)
